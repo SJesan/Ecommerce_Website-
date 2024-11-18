9c57b0c1bc6c1721a66897763fd587d43b96c31c
SJesan <sadikul.jesan@outlook.com>
2024-11-19 00:01:19 +0600

    new
    new

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        // Add other fonts here if needed
+      },
+    },
+  },
+  plugins: [],
+}
+
